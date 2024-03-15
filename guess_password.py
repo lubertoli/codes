28d68d725b336f510ca816b315dfd25ddcf8a2fc
lubertoli <96629311+lubertoli@users.noreply.github.com>
2024-03-14 18:54:36 -0600

    Delete guess_password.py

--- guess_password.py
+++
@@ -1,9 +0,0 @@
-actual_password = "MondayTuesdayWednesday"
-guess_password = ""
-
-while guess_password != actual_password:
-    guess_password = input("Input your password: ")
-    if actual_password == guess_password:
-        print("Account accessed.")
-    else:
-        print("Your password is incorrect.")
